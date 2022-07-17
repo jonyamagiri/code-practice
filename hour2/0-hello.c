c9fe85361e6e0a854e0a3f85a75d694ef1bb8209
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-07-17 16:12:35 +0300

    second C program

---
+++ hour2/0-hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Desc: program prints to stdout followed by a new line
+* Return: Always success (0)
+*/
+
+int main(void)
+{
+	printf("Howdy, neighbour!\nThis is my second C program.\n");
+	return (0);
+}
