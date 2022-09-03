1e51a279878f09b3d92fdd86cda54983d6775b5e
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-09-03 19:37:27 +0300

    Understanding Data Types and Keywords

---
+++ hour4/04L01.c
@@ -0,0 +1,14 @@
+/* 04L01.c: Printing out characters */
+#include <stdio.h>
+
+int main(void)
+{
+	char c1;
+	char c2;
+	
+	c1 = 'A';
+	c2 = 'a';
+	printf("Convert the value of c1 to character: %c.\n", c1);
+	printf("Convert the value of c2 to character: %c.\n", c2);
+	return (0);
+}
